c98059b36a3f2eac5d0698d92c72f4d1e8edcf23
hifromkevin <lam.kevin86@gmail.com>
2018-11-28 20:19:06 -0800

    Adds search component

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			term: ''
+		}
+
+		this.changeTerm = this.changeTerm.bind(this);
+		this.submitting = this.submitting.bind(this);
+	}
+
+	changeTerm(e) {
+		this.setState({
+			term: e.target.value
+		})
+	}
+
+	submitting(e) {
+		e.preventDefault();
+		this.props.getGifs('search', this.state.term);
+		this.setState({
+			term: ''
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={ this.submitting } >
+					<input value={ this.state.term } onChange={ this.changeTerm } /> <button>Search!</button>
+				</form>
+			</div>
+		)
+	}
+}
