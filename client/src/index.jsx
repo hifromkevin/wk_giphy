7c440d1eb43f957a8d6095087b671cfdc09213ec
hifromkevin <lam.kevin86@gmail.com>
2018-11-29 20:13:57 -0800

    Adds Bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import App from './components/App.jsx';
 import '../dist/styles/style.sass';
